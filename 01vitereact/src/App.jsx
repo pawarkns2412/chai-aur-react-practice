b45ac53a91d241913934686c23b003dd097c87ce
KomalPawar24 <pawarkns2412@gmail.com>
2024-04-23 15:43:20 +0530

    First Component in basic react and vite react projects

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,4 +1,4 @@
-
+import ViteFirstCompo from './ViteFirstCompo';
 
 function App() {
  
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1>"Hello Vite World"</h1>
+      <ViteFirstCompo/>
     </>
   )
 }
---
+++ 01vitereact/src/ViteFirstCompo.jsx
@@ -0,0 +1,4 @@
+function ViteFirstCompo() {
+    return(<h1>First Component in Vite React</h1>);
+} 
+export default ViteFirstCompo
